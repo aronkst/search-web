9de7e924a26ff81f08f661d961337c77f63c9b82
Aron <aron.0504@hotmail.com>
2022-09-14 21:39:49 -0300

    navbar, loading added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,23 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+document.addEventListener('turbo:visit', (_) => {
+  let modal = document.getElementById("modal-loading");
+  modal.classList.add("modal-open");
+})
+
+document.addEventListener('turbo:submit-start', (_) => {
+  let modal = document.getElementById("modal-loading");
+  modal.classList.add("modal-open");
+})
+
+document.addEventListener('turbo:render', (_) => {
+  var modal = document.getElementById("modal-loading");
+  modal.classList.remove("modal-open");
+})
+
+document.addEventListener('turbo:submit-end', (_) => {
+  var modal = document.getElementById("modal-loading");
+  modal.classList.remove("modal-open");
+})
